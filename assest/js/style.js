50551c0b984ea4bd12ee8010e9a0730e35c3d857
Kfriedman <Koryf91@gmail.com>
2019-10-25 22:38:18 -0700

    added more hmtl for the letters to be guessed. not working. manipulated my random word generator to use all four cat.

--- assest/js/style.js
+++ assest/js/style.js
@@ -1,38 +1,64 @@
+// checking to see the page is linked proper
+console.log("this is working?")
+
 var rand = 0;
-var word = "";
-var numWrong = 0;
-var numRight = 0;
-var phraseLength = 0;
+var word = " ";
 var phrases = ["airhead", "oh my god you are such a ditz", "bro that wave break was so knarly", "bad to the bone", "you bimbette", "totally tubular", "eat my shorts", "gag me with a spoon", "grody to the max", "take a chill pill", "that babe is totally bodacious", "don't have a cow", "party hardy with house music", "i don't have to take this. i'm going home", "i pitty the fool", "don't make me angry you wouldn't like me when i'm angry"];
 var cars = ["ford mustange gt 5.0", "dmc delorean", "ferrari testarossa", "porsche", "lamborghini countach", "chevrolet camero iroc-z", "audi quattro", "acura integra", "amc eagle", "buick grand national gnx", "datsun 280zx 10th anniversary edition", "dodge omni shelby glh", "mercedes-benz g class", "mitsubishi starion"];
 var movies = ["back to the future", "the breakfast club", "ghostbuster", "ferris buellers day off", "e.t. the extra-terrestrial", "the goonies", "raiders of the lost arc", "the terminator", "the princess bride", "star wars the empire strikes back", "gremlins", "blade runner", "beeteljuice", "beverly hills cop", "sixteen candles", " aliens", "airplane!", "stand by me", "bill and ted's excellent adventure", "the shining", "fast times at ridgemont high", "dirty dancing", "star wars return of the jedi", " who framed roger rabbit"];
 var music = ["take on me", "don't you want me", "don't you forget about me", "come on eileen", "time after time", "karma chameleon", "hungry like the wolf", "flashdance what a feeling", "total eclipse of the heart", "i wanna dane with somebody", "sweet dreams are made of these", "sweet child o' mine", "everybody wants to rule the", "eye of the tiger", "every breathe you take", "roxanne", "girls just want to have fun", "never gonna give you up", "another one bites the dust", "wake me up before you go go", "crazy little thing called love"];
-var challenges = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var word = null;
 var lettersinTheWord = [];
 var machedLetters = [];
-function randomWordGen() {
+function randomPhrases() {
     rand = Math.floor(Math.random() * phrases.length);
     word = phrases[rand];
+    console.log("RandomWord: " + word);
+    // lettersinTheWord = word.split("");
+    // displayWord()
+
+}
+randomPhrases();
+
+function ranMovies() {
+    rand = Math.floor(Math.random() * movies.length);
+    word = movies[rand];
+    console.log("RandomWord: " + word);
+    // lettersinTheWord = word.split("");
+    // displayWord()
+
+}
+ranMovies();
+function ranMusic() {
+    rand = Math.floor(Math.random() * music.length);
+    word = music[rand];
+    console.log("RandomWord: " + word);
+    // lettersinTheWord = word.split("");
+    // displayWord()
+
+}
+ranMusic();
+function ranCars() {
+    rand = Math.floor(Math.random() * cars.length);
+    word = cars[rand];
     console.log("RandomWord: " + word);
     lettersinTheWord = word.split("");
     displayWord()
 
 }
-randomWordGen();
+ranCars();
 
-function displayWord(){
-    var PhraseView = "";
-    for(var i=0; i<lettersinTheWord.length; i++){
-        //if the current letter has been guseed , display that letter
-        if(machedLetters.indexOf(lettersinTheWord[i]) !== -1){
-            PhraseView +=lettersinTheWord[i];
-        }
-        else{
-            wordView += "&nbsp;_&nbsp;"
-        }
-    }
-    document.querySelector(".difficulties_page").innerHTML = wordView;
 
-}
+// function displayWord(){
+//     var PhraseView = "";
+//     // for(var i=0; i<lettersinTheWord.length; i++){
+//         //if the current letter has been guseed , display that letter
+//         if(machedLetters.indexOf(lettersinTheWord[i]) !== -1){
+//             PhraseView +=lettersinTheWord[i];
+//         }
+//         else{
+//             wordView += "&nbsp;_&nbsp;"
+//         }
+//     }
+//     document.querySelector(".difficulties_page").innerHTML = wordView;
+
