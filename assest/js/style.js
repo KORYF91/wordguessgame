c61fd97146cbb81bad1692182a58c64c49798205
Kfriedman <Koryf91@gmail.com>
2019-10-26 17:22:40 -0700

    working on displaying underlines

--- assest/js/style.js
+++ assest/js/style.js
@@ -7,9 +7,10 @@ var phrases = ["airhead", "oh my god you are such a ditz", "bro that wave break 
 var cars = ["ford mustange gt 5.0", "dmc delorean", "ferrari testarossa", "porsche", "lamborghini countach", "chevrolet camero iroc-z", "audi quattro", "acura integra", "amc eagle", "buick grand national gnx", "datsun 280zx 10th anniversary edition", "dodge omni shelby glh", "mercedes-benz g class", "mitsubishi starion"];
 var movies = ["back to the future", "the breakfast club", "ghostbuster", "ferris buellers day off", "e.t. the extra-terrestrial", "the goonies", "raiders of the lost arc", "the terminator", "the princess bride", "star wars the empire strikes back", "gremlins", "blade runner", "beeteljuice", "beverly hills cop", "sixteen candles", " aliens", "airplane!", "stand by me", "bill and ted's excellent adventure", "the shining", "fast times at ridgemont high", "dirty dancing", "star wars return of the jedi", " who framed roger rabbit"];
 var music = ["take on me", "don't you want me", "don't you forget about me", "come on eileen", "time after time", "karma chameleon", "hungry like the wolf", "flashdance what a feeling", "total eclipse of the heart", "i wanna dane with somebody", "sweet dreams are made of these", "sweet child o' mine", "everybody wants to rule the", "eye of the tiger", "every breathe you take", "roxanne", "girls just want to have fun", "never gonna give you up", "another one bites the dust", "wake me up before you go go", "crazy little thing called love"];
-var word = null;
+// var word = null;
 var lettersinTheWord = [];
-var machedLetters = [];
+var matchedLetters = [];
+
 function randomPhrases() {
     rand = Math.floor(Math.random() * phrases.length);
     word = phrases[rand];
@@ -29,6 +30,7 @@ function ranMovies() {
 
 }
 ranMovies();
+
 function ranMusic() {
     rand = Math.floor(Math.random() * music.length);
     word = music[rand];
@@ -38,27 +40,28 @@ function ranMusic() {
 
 }
 ranMusic();
+
 function ranCars() {
     rand = Math.floor(Math.random() * cars.length);
     word = cars[rand];
     console.log("RandomWord: " + word);
     lettersinTheWord = word.split("");
     displayWord()
-
 }
 ranCars();
 
 
-// function displayWord(){
-//     var PhraseView = "";
-//     // for(var i=0; i<lettersinTheWord.length; i++){
-//         //if the current letter has been guseed , display that letter
-//         if(machedLetters.indexOf(lettersinTheWord[i]) !== -1){
-//             PhraseView +=lettersinTheWord[i];
-//         }
-//         else{
-//             wordView += "&nbsp;_&nbsp;"
-//         }
-//     }
-//     document.querySelector(".difficulties_page").innerHTML = wordView;
+ function displayWord(){
+     var PhraseView = "";
+for (var i=0; i<lettersinTheWord.length; i++){
+//the current letter has been guseed , display that letter
+    if ("#guessletter".indexOf(lettersinTheWord[i]) !== -1)
+        phraseView +=lettersinTheWord[i];        
+   else 
+           wordView += "&nbsp;_&nbsp;"
+           console.log(displayWord)
+       }
+   }
+displayWord();
+//   document.querySelector(".difficulties_page").innerHTML = wordView;
 
